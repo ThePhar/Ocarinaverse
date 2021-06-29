83828ee1a760418864e13faf862156b4d9d0e149
Zach Parks <phar@Zachs-MacBook-Air.local>
2021-06-28 22:30:49 -0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * The is the main function that is called at the start of our application. This is so we can have async-await in our
+ * starting code.
+ */
+async function main() {
+  // Test code, ignore this.
+  console.log("Hello, world!");
+}
+
+// Our main entry point starts here.
+void main();
